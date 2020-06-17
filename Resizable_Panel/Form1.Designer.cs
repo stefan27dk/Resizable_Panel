c74a2dd8a2a5317ec62d449d7f2d2b3d72181fe7
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-06-17 14:27:19 +0200

    Add project files.

---
+++ Resizable_Panel/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Resizable_Panel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Resize_panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Resize_panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Resize_panel1
+            // 
+            this.Resize_panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Resize_panel1.Controls.Add(this.button1);
+            this.Resize_panel1.Location = new System.Drawing.Point(470, 115);
+            this.Resize_panel1.Name = "Resize_panel1";
+            this.Resize_panel1.Size = new System.Drawing.Size(214, 185);
+            this.Resize_panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.panel2.Location = new System.Drawing.Point(245, 148);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(122, 114);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.Resize_panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize_panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Resize_panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Resizable_Panel/My_Resizable_Panel.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ControlManager
+{
+    internal class ControlMoverOrResizer
+    {
+        private static bool _moving;
+        private static Point _cursorStartPoint;
+        private static bool _moveIsInterNal;
+        private static bool _resizing;
+        private static Size _currentControlStartSize;
+        internal static bool MouseIsInLeftEdge { get; set; }
+        internal static bool MouseIsInRightEdge { get; set; }
+        internal static bool MouseIsInTopEdge { get; set; }
+        internal static bool MouseIsInBottomEdge { get; set; }
+
+
+
+
+        // Body
+        internal enum MoveOrResize     // Enum is Reffereing to names with digits like  "Reffering to names with digits" like in a list
+        {
+            Move,  // Is "0"
+            Resize, // Is "1"
+            MoveAndResize  // Is "2"
+        }
+
+
+
+        // Accesor
+        internal static MoveOrResize WorkType { get; set; }
+
+
+
+
+
+
+        // Main Method
+        internal static void Init(Control control) 
+        {
+            Init(control, control);
+        }
+
+
+
+         // Control and container control is the one that is dragable anf the container is the parretn of the dragable control so EX: drag container only when you drag the button inside the container
+        internal static void Init(Control control, Control container) // Initialize // Settings for the Selected Control
+        {
+            _moving = false;
+            _resizing = false;
+            _moveIsInterNal = false;
+            _cursorStartPoint = Point.Empty;
+            MouseIsInLeftEdge = false;
+            MouseIsInLeftEdge = false;
+            MouseIsInRightEdge = false;
+            MouseIsInTopEdge = false;
+            MouseIsInBottomEdge = false;
+            WorkType = MoveOrResize.MoveAndResize;
+            control.MouseDown += (sender, e) => StartMovingOrResizing(control, e);  //#### Setts Mouse Events to the selected Control  
+            control.MouseUp += (sender, e) => StopDragOrResizing(control);
+            control.MouseMove += (sender, e) => MoveControl(container, e);
+        }
+
+
+
+
+
+
+        private static void UpdateMouseEdgeProperties(Control control, Point mouseLocationInControl)
+        {
+            if (WorkType == MoveOrResize.Move)
+            {
+                return;
+            }
+
+             //## EDGES of the Control
+
+            // Booleans: Mouse is in EDGE 
+            MouseIsInLeftEdge = Math.Abs(mouseLocationInControl.X) <= 10;
+            MouseIsInRightEdge = Math.Abs(mouseLocationInControl.X - control.Width) <= 10;
+            MouseIsInTopEdge = Math.Abs(mouseLocationInControl.Y) <= 2;
+            MouseIsInBottomEdge = Math.Abs(mouseLocationInControl.Y - control.Height) <= 10;
+        }
+
+
+
+
+
+        // Change Cursor Types if mouse is in some of the EDGES
+        private static void UpdateMouseCursor(Control control)
+        {
+            if (WorkType == MoveOrResize.Move)   // If is move return and check again if it is moving
+            {
+                return;
+            }
+
+
+            if (MouseIsInLeftEdge)
+            {
+                if (MouseIsInTopEdge)
+                {
+                    control.Cursor = Cursors.SizeNWSE;   // Change Currsor type to Diagonal with two arrows
+                }
+                else if (MouseIsInBottomEdge)
+                {
+                    control.Cursor = Cursors.SizeNESW;
+                }
+                else
+                {
+                    control.Cursor = Cursors.SizeWE;
+                }
+            }
+
+
+
+            else if (MouseIsInRightEdge)
+            {
+                if (MouseIsInTopEdge)
+                {
+                    control.Cursor = Cursors.SizeNESW;
+                }
+                else if (MouseIsInBottomEdge)
+                {
+                    control.Cursor = Cursors.SizeNWSE;
+                }
+                else
+                {
+                    control.Cursor = Cursors.SizeWE;
+                }
+            }
+
+
+            else if (MouseIsInTopEdge || MouseIsInBottomEdge)
+            {
+                control.Cursor = Cursors.SizeNS;
+            }
+            else
+            {
+                control.Cursor = Cursors.Default;
+            }
+        }
+
+
+
+
+
+        // Mouse Down::
+        private static void StartMovingOrResizing(Control control, MouseEventArgs e)
+        {
+            if (_moving || _resizing)  // If Moving or Resizing Return and check again
+            {
+                return;
+            }
+
+
+              // Resizing
+            if (WorkType != MoveOrResize.Move && (MouseIsInRightEdge || MouseIsInLeftEdge || MouseIsInTopEdge || MouseIsInBottomEdge))
+            {
+                _resizing = true;
+                _currentControlStartSize = control.Size; // Control Size before Resizing
+            }
+
+
+             // Move
+            else if (WorkType != MoveOrResize.Resize)
+            {
+                _moving = true;
+                control.Cursor = Cursors.Hand;
+            }
+            _cursorStartPoint = new Point(e.X, e.Y);
+            control.Capture = true;
+
+
+        }
+
+
+
+
+
+
+        // Mouse Move:: in Control
+        private static void MoveControl(Control control, MouseEventArgs e)   // Move EVENT of the Control
+        {
+
+
+            if (!_resizing && !_moving) // If it is not resizing or Moving Update Edge Properties and check if the mouse is in an EDGE Position so you can change the cursor 
+            {
+                UpdateMouseEdgeProperties(control, new Point(e.X, e.Y)); // Look up for if the mouse is in edge position
+                UpdateMouseCursor(control); // Change Cursor if mouse is in an Edge position
+            }
+
+
+
+            if (_resizing)
+            {
+                if (MouseIsInLeftEdge)
+                {
+                    if (MouseIsInTopEdge)
+                    {
+                        control.Width -= (e.X - _cursorStartPoint.X);
+                        control.Left += (e.X - _cursorStartPoint.X);
+                        control.Height -= (e.Y - _cursorStartPoint.Y);
+                        control.Top += (e.Y - _cursorStartPoint.Y);
+                    }
+                    else if (MouseIsInBottomEdge)
+                    {
+                        control.Width -= (e.X - _cursorStartPoint.X);
+                        control.Left += (e.X - _cursorStartPoint.X);
+                        control.Height = (e.Y - _cursorStartPoint.Y) + _currentControlStartSize.Height;
+                    }
+                    else
+                    {
+                        control.Width -= (e.X - _cursorStartPoint.X);
+                        control.Left += (e.X - _cursorStartPoint.X);
+                    }
+                }
+
+
+
+                else if (MouseIsInRightEdge)
+                {
+                    if (MouseIsInTopEdge)
+                    {
+                        control.Width = (e.X - _cursorStartPoint.X) + _currentControlStartSize.Width;
+                        control.Height -= (e.Y - _cursorStartPoint.Y);
+                        control.Top += (e.Y - _cursorStartPoint.Y);
+
+                    }
+                    else if (MouseIsInBottomEdge)
+                    {
+                        control.Width = (e.X - _cursorStartPoint.X) + _currentControlStartSize.Width;
+                        control.Height = (e.Y - _cursorStartPoint.Y) + _currentControlStartSize.Height;
+                    }
+                    else
+                    {
+                        control.Width = (e.X - _cursorStartPoint.X) + _currentControlStartSize.Width;
+                    }
+                }
+
+
+
+                else if (MouseIsInTopEdge)
+                {
+                    control.Height -= (e.Y - _cursorStartPoint.Y);
+                    control.Top += (e.Y - _cursorStartPoint.Y);
+                }
+                else if (MouseIsInBottomEdge)
+                {
+                    control.Height = (e.Y - _cursorStartPoint.Y) + _currentControlStartSize.Height;
+                }
+                else
+                {
+                    StopDragOrResizing(control);
+                }
+            }
+
+
+
+            else if (_moving)
+            {
+                _moveIsInterNal = !_moveIsInterNal;
+                if (!_moveIsInterNal)
+                {
+                    int x = (e.X - _cursorStartPoint.X) + control.Left;
+                    int y = (e.Y - _cursorStartPoint.Y) + control.Top;
+                    control.Location = new Point(x, y);
+                }
+            }
+
+
+        }
+
+
+
+
+
+
+        private static void StopDragOrResizing(Control control)
+        {
+            _resizing = false;
+            _moving = false;
+            control.Capture = false;
+            UpdateMouseCursor(control);
+        }
+
+
+
+
+
+
+        //#region Save And Load
+
+        //private static List<Control> GetAllChildControls(Control control, List<Control> list)
+        //{
+        //    List<Control> controls = control.Controls.Cast<Control>().ToList();
+        //    list.AddRange(controls);
+        //    return controls.SelectMany(ctrl => GetAllChildControls(ctrl, list)).ToList();
+        //}
+
+        //internal static string GetSizeAndPositionOfControlsToString(Control container)
+        //{
+        //    List<Control> controls = new List<Control>();
+        //    GetAllChildControls(container, controls);
+        //    CultureInfo cultureInfo = new CultureInfo("en");
+        //    string info = string.Empty;
+        //    foreach (Control control in controls)
+        //    {
+        //        info += control.Name + ":" + control.Left.ToString(cultureInfo) + "," + control.Top.ToString(cultureInfo) + "," +
+        //                control.Width.ToString(cultureInfo) + "," + control.Height.ToString(cultureInfo) + "*";
+        //    }
+        //    return info;
+        //}
+        //internal static void SetSizeAndPositionOfControlsFromString(Control container, string controlsInfoStr)
+        //{
+        //    List<Control> controls = new List<Control>();
+        //    GetAllChildControls(container, controls);
+        //    string[] controlsInfo = controlsInfoStr.Split(new[] { "*" }, StringSplitOptions.RemoveEmptyEntries);
+        //    Dictionary<string, string> controlsInfoDictionary = new Dictionary<string, string>();
+        //    foreach (string controlInfo in controlsInfo)
+        //    {
+        //        string[] info = controlInfo.Split(new[] { ":" }, StringSplitOptions.RemoveEmptyEntries);
+        //        controlsInfoDictionary.Add(info[0], info[1]);
+        //    }
+        //    foreach (Control control in controls)
+        //    {
+        //        string propertiesStr;
+        //        controlsInfoDictionary.TryGetValue(control.Name, out propertiesStr);
+        //        string[] properties = propertiesStr.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+        //        if (properties.Length == 4)
+        //        {
+        //            control.Left = int.Parse(properties[0]);
+        //            control.Top = int.Parse(properties[1]);
+        //            control.Width = int.Parse(properties[2]);
+        //            control.Height = int.Parse(properties[3]);
+        //        }
+        //    }
+        //}
+
+        //#endregion
+    }
+}
